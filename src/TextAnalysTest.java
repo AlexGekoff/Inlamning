5d34c1b1eaeb281286d9dacfb474429dfd7fe16d
Aleksandra <aleksandra.gekoff@gmail.com>
2024-11-12 09:17:04 +0100

    Har skapat ett JUnit test som kontrollerar att metoden GetLineCounts fungerar korrekt

--- src/TextAnalysTest.java
+++ src/TextAnalysTest.java
@@ -5,12 +5,26 @@ public class TextAnalysTest {
 
     @Test
 
-    void testGetTotalCharactersInput() {
+    void testGetTotalCharacters() {
         TextAnalysMain textAnalysMain = new TextAnalysMain();         // Skapar TextAnalysMain-objekt
         textAnalysMain.addLine("Hello");
         textAnalysMain.addLine("World");
 
         assertEquals(10, textAnalysMain.getTotalCharacters(), "Antalet tecken borde vara 10 när vi har lagt till 'Hello' och 'World'");
+
+    }
+
+    @Test
+
+    void testGetTotalLine() {
+        TextAnalysMain textAnalysMain = new TextAnalysMain();
+        textAnalysMain.addLine("Här kontrolerar vi");
+        textAnalysMain.addLine("hur det fungerar");
+        textAnalysMain.addLine("med en metod");
+
+        assertEquals(3, textAnalysMain.getLineCount());
+
+
     }
 
 
